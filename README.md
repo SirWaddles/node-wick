96b3008636debab166c3cd9d2db6cb61d71a611a
Waddlesworth <github@genj.io>
2019-07-24 15:59:10 +1000

    new Package class instead of functions

--- README.md
+++ README.md
@@ -21,24 +21,39 @@ extractor.get_file_list().forEach((v, idx) => {
 });
 ```
 
+## Parsing
+
+The `Package` class lets you parse files uasset/uexp files. It has two constructors:
+```javascript
+const { Package } = require('node-wick');
+
+// Construct a package from a filepath
+let asset = new Package("./assets/test");
+
+// Construct a package from several buffers
+// Note that the ubulk buffer is optional, but the parser will error if you try to parse an asset that requires it.
+let uassset_buf = fs.readFileSync("./assets/test.uasset");
+let uexp_buf = fs.readFileSync("./assets/test.uexp");
+let ubulk_buf = fs.readFileSync("./assets/test.ubulk");
+let asset = new Package(uasset_buf, uexp_buf, ubulk_buf);
+```
+
+You can get a JSON object of the internal parameters of the objects, using the `get_data` method:
+
+```javascript
+let asset = new Package("./assets/test");
+console.log(JSON.stringify(asset.get_data()));
+```
+
+You can also get a buffer of the Texture contained within the asset (if there is one) using the `get_texture` method:
+
+```javascript
+let asset = new Package("./assets/test");
+fs.writeFileSync("./assets/test.png", asset.get_texture());
+```
+
+Note that retrieving the texture from a Package asset, will invalidate the package (it can no longer be used.)
+
 ### Pak Key GUID
 
 The `read_pak_key(path: string)` will return the `EncryptionKeyGuid` part of the header inside the pak file. You do not need the key to get this data.
-
-## Parsing Files
-
-The `read_asset(path: string)` function is the main way to parse an asset file.
-
-The path parameter **should not** contain a file extension - it will load up both the uexp and uasset files.
-
-This function returns a JavaScript object representation of the underlying UObject data. It will be an array of exports, each with the `export_type` parameter.
-
-## Textures
-
-There are two functions to read textures:
- * `read_texture(path: string)`
- * `read_texture_to_file(path: string, output_path: string)`
-
-Both of these functions accept a path of the same format as `read_asset`, and will throw an exception if the `export_type` of the first export is not a `Texture2D`
-
-While `read_texture` returns a NodeJS Buffer containing png data, `read_texture_to_file` will write that content directly to the `output_path` provided.
--- native/src/lib.rs
+++ native/src/lib.rs
@@ -6,6 +6,7 @@ extern crate john_wick_parse;
 use neon::prelude::*;
 use std::fs;
 use std::io::Write;
+use std::cell::Cell;
 use john_wick_parse::{assets, read_texture as read_texture_asset};
 use john_wick_parse::archives::PakExtractor;
 
@@ -13,39 +14,14 @@ fn parse_err(err: assets::ParserError) -> String {
     err.get_properties().into_iter().rev().fold(String::new(), |acc, v| acc + "\n" + v)
 }
 
-fn read_asset(mut cx: FunctionContext) -> JsResult<JsValue> {
-    let asset_path = cx.argument::<JsString>(0)?.value();
-    let package = match assets::Package::from_file(&asset_path) {
-        Ok(data) => data,
-        Err(err) => return cx.throw_error(parse_err(err)),
-    };
+fn get_buffer_contents(cx: &mut MethodContext<JsUndefined>, buffer: Handle<JsBuffer>) -> Vec<u8> {
+    let guard = cx.lock();
+    let contents = buffer.borrow(&guard);
+    let slice = contents.as_slice();
+    let mut buffer_data = vec![0u8; slice.len()];
+    buffer_data.as_mut_slice().copy_from_slice(slice);
 
-    let js_asset = neon_serde::to_value(&mut cx, &package)?;
-    Ok(js_asset)
-}
-
-fn read_texture(mut cx: FunctionContext) -> JsResult<JsValue> {
-    let asset_path = cx.argument::<JsString>(0)?.value();
-    let package = match assets::Package::from_file(&asset_path) {
-        Ok(data) => data,
-        Err(err) => return cx.throw_error(parse_err(err)),
-    };
-
-    let texture_data = match read_texture_asset(package) {
-        Ok(data) => data,
-        Err(err) => return cx.throw_error(parse_err(err)),
-    };
-
-    let tex_buffer = {
-        let mut buffer = JsBuffer::new(&mut cx, texture_data.len() as u32)?;
-        let guard = cx.lock();
-        let mut contents = buffer.borrow(&guard);
-        let mut slice = contents.as_mut_slice();
-        slice.copy_from_slice(&texture_data);
-        buffer
-    };
-
-    Ok(tex_buffer.upcast())
+    buffer_data
 }
 
 fn read_texture_to_file(mut cx: FunctionContext) -> JsResult<JsValue> {
@@ -75,6 +51,10 @@ fn read_pak_key(mut cx: FunctionContext) -> JsResult<JsString> {
     };
 
     Ok(JsString::new(&mut cx, header.get_key_guid().to_string()))
+}
+
+pub struct Package {
+    package: Cell<assets::Package>,
 }
 
 declare_types! {
@@ -137,13 +117,104 @@ declare_types! {
             Ok(JsString::new(&mut cx, mount_point).upcast())
         }
     }
+
+    pub class JsPackage for Package {
+        init(mut cx) {
+            match cx.len() {
+                1 => {
+                    let asset_path = cx.argument::<JsString>(0)?.value();
+                    let package = match assets::Package::from_file(&asset_path) {
+                        Ok(data) => data,
+                        Err(err) => return cx.throw_error(parse_err(err)),
+                    };
+                    Ok(Package {
+                        package: Cell::new(package),
+                    })
+                },
+                _ => {
+                    let uasset_js = cx.argument::<JsBuffer>(0)?;
+                    let uexp_js = cx.argument::<JsBuffer>(1)?;
+                    let uasset = get_buffer_contents(&mut cx, uasset_js);
+                    let uexp = get_buffer_contents(&mut cx, uexp_js);
+
+                    let ubulk_js = match cx.argument_opt(2) {
+                        Some(arg) => Some(arg.downcast_or_throw(&mut cx)?),
+                        None => None,
+                    };
+                    let ubulk = match ubulk_js {
+                        Some(buffer) => Some(get_buffer_contents(&mut cx, buffer)),
+                        None => None,
+                    };
+
+                    let package = match assets::Package::from_buffer(uasset, uexp, ubulk) {
+                        Ok(data) => data,
+                        Err(err) => return cx.throw_error(parse_err(err)),
+                    };
+                    Ok(Package {
+                        package: Cell::new(package),
+                    })
+                }
+            }
+        }
+
+        method get_data(mut cx) {
+            /*
+             * Okay so this is stupid
+             * 
+             * Neon's ref locks the internal value quite a bit. I need a mutable borrow on the CallContext in order
+             * to generate the JS serialization. According to the documentation the solution seems to be "just clone it lol"
+             * Interior mutability seemed like the only solution, but since no reference can outlast the lock, that leaves
+             * std::cell::Cell to the rescue. But I needed something to put back in the Cell while I'm using it. 
+             * Hence the incredibly dumb ::empty function
+             * 
+             * If there's a far better way of doing this that I'm being ignorant of, please let me know.
+             */
+            let this = cx.this();
+            let package = {
+                let guard = cx.lock();
+                let data = this.borrow(&guard);
+                data.package.replace(assets::Package::empty())
+            };
+            let js_data = neon_serde::to_value(&mut cx, &package)?;
+            {
+                let guard = cx.lock();
+                let data = this.borrow(&guard);
+                data.package.set(package);
+            }
+            Ok(js_data)
+        }
+
+        method get_texture(mut cx) {
+            let this = cx.this();
+            let package = {
+                let guard = cx.lock();
+                let data = this.borrow(&guard);
+                data.package.replace(assets::Package::empty())
+            };
+
+            let texture_data = match read_texture_asset(package) {
+                Ok(data) => data,
+                Err(err) => return cx.throw_error(parse_err(err)),
+            };
+
+            let tex_buffer = {
+                let mut buffer = JsBuffer::new(&mut cx, texture_data.len() as u32)?;
+                let guard = cx.lock();
+                let mut contents = buffer.borrow(&guard);
+                let mut slice = contents.as_mut_slice();
+                slice.copy_from_slice(&texture_data);
+                buffer
+            };
+
+            Ok(tex_buffer.upcast())
+        }
+    }
 }
 
 register_module!(mut cx, {
-    cx.export_function("read_asset", read_asset)?;
-    cx.export_function("read_texture", read_texture)?;
     cx.export_function("read_texture_to_file", read_texture_to_file)?;
     cx.export_function("read_pak_key", read_pak_key)?;
     cx.export_class::<JsPakExtractor>("PakExtractor")?;
+    cx.export_class::<JsPackage>("Package")?;
     Ok(())
 });
